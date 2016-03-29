dafced5a4e007dae7b4962aa2e0bab955758b1d6
Jakub Klapacz <kubaklapacz@gmail.com>
2016-03-29 15:23:07 -0500

    Apriori works, closed mining about to be added

--- hw3/data-assign3/apriori.py
+++ hw3/data-assign3/apriori.py
@@ -1,7 +1,7 @@
 from sys import argv
 from infra import *
 usage = "python apriori.py [input filename] [output filename] [min-sup (between 0 and 1)]"
-
+from time import clock
 
 
 
@@ -9,12 +9,14 @@ usage = "python apriori.py [input filename] [output filename] [min-sup (between 
 def main(infile, outfile, min_sup):
 	a = apriori(infile, outfile, min_sup)
 	a.process_input()
-	print a
 	# a.generate_itemset(0)
-	a.patter_mining()
+	a.pattern_mining()
+	a.closed_mining()
 if __name__ == '__main__':
 	if len(argv) != 4:
 		print usage
 		quit()
+	now = clock()
 	main(argv[1], argv[2], argv[3])
-
+	elapsed = clock() - now
+	print "Apriori ran in [" + str(elapsed) + "] seconds"
--- hw3/data-assign3/infra.py
+++ hw3/data-assign3/infra.py
@@ -1,7 +1,10 @@
 from collections import defaultdict
 from copy import copy
+from operator import itemgetter
 
-debug = True
+debug = False	
+vocabfile = "data-assign3/vocab.txt"
+
 
 class apriori:
 	def __init__(self, infile, outfile, min_sup):
@@ -10,46 +13,59 @@ class apriori:
 		self.support = (float)(min_sup)
 		self.total_count = 0
 		self.k_itemsets = list()
-		self.file_lines = None
-	def __str__(self):
-		return str(len(self.k_itemsets))
+		self.transactions = list()
+		self.vocab = self.init_vocab()
+		self.pattern_list = None
+		self.closed_list = list()
+		self.max_list = list()
+
+	def init_vocab(self):
+		output = {}
+		f = open(vocabfile)
+		for line in f.readlines():
+			l = line.split()
+			key = int(l[0])
+			output[key] = l[1]
+		return output
+
+			
+
 
 	def prune(self, itemset):
 		pruned = {}
 		for element in itemset:
 			count = itemset[element]
-			if self.support < 1:
-				element_support = (float)((float)(count) / (float)(self.total_count))
-			else:
-				element_support = count
-			if element_support > self.support:
+			if self.meets_support(count):
 				pruned[element] = count
 		return pruned
 
+	def meets_support(self, support):
+		if self.support < 1:
+			element_support = (float)((float)(support) / (float)(self.total_count))
+		else:
+			element_support = support
+		return element_support > self.support
+
 	def process_input(self):
 		f = open(self.infile)
-		self.file_lines = f.readlines()
+		lines = f.readlines()
 		f.close()
-		self.total_count = len(self.file_lines)
+		self.total_count = len(lines)
 		one_itemset_pre = defaultdict(int)
 		one_itemset_pruned = {}
-		for line in self.file_lines:
+		for line in lines:
 			curr_line = line.split()
+			transaction = [int(x) for x in curr_line]
+			transaction = set(transaction)
+			self.transactions.append(transaction)
 			for element in curr_line:
 				e = int(element)
 				one_itemset_pre[e] += 1
 		if debug: print len(one_itemset_pre)
-		# for element in one_itemset_pre:
-		# 	count = one_itemset_pre[element]
-		# 	if self.support < 1:
-		# 		element_support = (float)((float)(count) / (float)(self.total_count))
-		# 	else:
-		# 		element_support = count
-		# 	if element_support > self.support:
-		# 		one_itemset_pruned[element] = count
 		one_itemset_pruned = self.prune(one_itemset_pre)
 		if debug: print len(one_itemset_pruned)
 		self.k_itemsets.append(one_itemset_pruned)
+
 	
 	def generate_itemset(self, k):
 		k_plus_one = defaultdict(int)
@@ -59,8 +75,9 @@ class apriori:
 		temp = copy(self.k_itemsets[0])
 		generated = False
 		for element in k_itemset:
-			counter += 1
-			print counter
+			if debug:
+				counter += 1
+				print counter
 			if k == 0:
 				temp_list = list()
 				temp_list.append(element)
@@ -68,11 +85,11 @@ class apriori:
 				temp_list = list(element)
 			# temp = copy(k_itemset)
 			for i in temp_list:
-				print "item" + str(i)
+				if debug: print "item" + str(i)
 				if i in temp:
 					del temp[i]
 			# del temp[element]
-			print len(temp)
+			if debug: print len(temp)
 			for item in temp:
 				
 				# if k == 0:
@@ -83,58 +100,109 @@ class apriori:
 
 				new_element = temp_list + inner_temp_list
 				new_tuple = tuple(new_element)
-				
-				for line in self.file_lines:
-					curr_line = line.split()
-					numbers = [int(x) for x in curr_line]
-					# print numbers
-					# print new_element
-					if set(new_element) <= set(numbers):
+				for transaction in self.transactions:
+					if set(new_element) <= transaction:
 						generated = True
-						# print "!"
-						# print numbers
-						# print new_element
-						# print new_tuple
-						# quit()
 						k_plus_one[new_tuple] += 1
+				# if self.meets_support(k_plus_one[new_tuple]) == False:
+					# del k_plus_one[new_tuple]
 
-			# print len(k_itemset)
-			# print len(k_plus_one)
-			# print k_plus_one
-			# quit()
-			# print len(temp)
 		if generated == False:
 			return False
 		pruned = self.prune(k_plus_one)
+		if len(pruned) == 0:
+			return False
 		self.k_itemsets.append(pruned)
 		return True
-		# print len(k_itemset)
-		# print len(k_plus_one)
-		# print k_itemset
-		# print k_plus_one
-			# quit()			
 
-	def patter_mining(self):
+	def output_rules(self):
+		if self.pattern_list is None:
+			output = list()
+			for i in range(len(self.k_itemsets)):
+				curr_dict = self.k_itemsets[i]
+				for element in self.k_itemsets[i]:
+					item = (element, curr_dict[element])
+					output.append(item)
+			output = sorted(output, key=itemgetter(1), reverse=True)
+			self.pattern_list = output
+		outputstr = ''
+		if self.support < 1:
+			outputstr += "Minimum support = {} (approx. {:.0f})".format(self.support, self.total_count*self.support)
+		else:
+			outputstr += "Minimum support = {}".format(self.support)
+		outputstr += "\nSUPPORT | FREQUENCY | RULE\n"
+		for i in range(len(self.pattern_list)):
+			
+			t = self.pattern_list[i]
+			translate = None
+			if type(t[0]) is tuple:
+				translate = list(t[0])
+			frequency = float(t[1]) / float(self.total_count)
+			outputstr += "{}\t{:.3f}\t".format(t[1], frequency)
+
+			if translate is not None:
+				for candidate in translate:
+					
+					outputstr += self.vocab[candidate]  
+					
+			else:
+				outputstr += self.vocab[t[0]]
+			outputstr += '\n'
+		print outputstr
+		# outf = open(self.outfile, 'w')
+		# outf.write(outputstr)
+		# outf.close()
+
+		# print outputstr
+
+			# temp_list = list(t[0])
+			# print temp_list
+
+			# print output[i]
+			# quit()
+	def closed_mining(self):
+		for i in range(len(self.pattern_list)):
+			add_to_closed = True
+			current = self.pattern_list[i]
+			sub = current[0]
+			if type(sub) is int:
+				l = list()
+				l.append(sub)
+				sub = set(l)
+			else:
+				sub = set(sub)
+			for j in range(len(self.pattern_list)):
+				candidate = self.pattern_list[j]
+				sup = candidate[0]
+				if type(sup) is int:
+					t = list()
+					t.append(sup)
+					sup = set(t)
+				else:
+					sup = set(sup)
+				if sup > sub and current[1] == candidate[1]:
+					add_to_closed = False
+			if add_to_closed == True:
+				self.closed_list.append(current)
+		self.output_rules(self.closed_list)
+
+			
+
+
+	def pattern_mining(self):
 		curr_itemset = 0
 		while(True):
+			print "Generating " + str(curr_itemset + 1) + "-itemset"
 			escape = self.generate_itemset(curr_itemset) 
 			curr_itemset += 1
 			if escape == False:
 				break
 
-		print len(self.k_itemsets)
-		for itemset in self.k_itemsets:
-			print itemset
+		self.output_rules(self.pattern_list)
+		# self.closed_mining()
 
+		if debug:
+			print len(self.k_itemsets)
+			for itemset in self.k_itemsets:
+				print itemset
 
-	# 	test_list = list()
-	# 	test_list.append('4')
-	# 	test_list.append('6')
-	# 	for line in self.file_lines:
-	# 		curr_line = line.split()
-	# 		print curr_line
-	# 		print test_list
-	# 		# quit()
-	# 		if set(test_list) <= set(curr_line):
-	# 			print "!"
-	# 			quit()
